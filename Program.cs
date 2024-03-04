7248025029fbb78f03f8fe9d2548568fa3260319
Mila <147173617+Ethrusk@users.noreply.github.com>
2024-03-04 20:53:51 +0300

    Final Test Code added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите элементы массива через пробел:");
+        string? input = Console.ReadLine();
+        string[] inputArray = input!.Split( );
+
+        string[] filteredArray = new string[inputArray.Length];
+        int resultIndex = 0;
+
+        foreach (string str in inputArray)
+        {
+            if (str.Length <= 3)
+            {
+                filteredArray[resultIndex] = str;
+                resultIndex++;
+            }
+        }
+
+        Array.Resize(ref filteredArray, resultIndex);
+
+        
+        Console.WriteLine("Результат:");
+        foreach (string str in filteredArray)
+        {
+            Console.Write(str);
+            Console.Write(", ");
+        }
